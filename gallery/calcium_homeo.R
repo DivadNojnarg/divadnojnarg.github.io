d30bc0c4c81ef8b76c5d8c1ec86f2ddf24318506
Divad0175 <dgranjon@ymail.com>
2017-05-21 19:48:54 +0200

    major update

---
+++ gallery/calcium_homeo.R
@@ -0,0 +1,9 @@
++++
+  date = "2017-05-10T21:35:17-07:00"
+  title = "Shiny app of the Lorenz model"
+  image = "lorenz_attractor.png"
+  alt = "This is a description of the image"
+  color = ""
+  link1 = ""
+  link2 = ""
+  +++
